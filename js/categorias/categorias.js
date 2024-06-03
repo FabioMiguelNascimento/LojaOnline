5bb94ea56bfa86a7a074956e63749cd016d1bcef
Fabio Miguel <fabiomnascimento05@gmail.com>
2024-06-03 16:13:26 -0300

    Adicionado pesquisa por nome e categorias no header

--- js/categorias/categorias.js
+++ js/categorias/categorias.js
@@ -1,33 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Mapeamento de tags para nomes de categorias
     const categoriaNomes = {
-        "eletronicos": "Eletrônicos",
-        "eletrodomesticos": "Eletrodomésticos",
-        "computador": "Computadores",
-        "celular": "Celulares",
-        "tech": "Tecnologias",
-        "casa": "Casa",
-        "banheiro": "Banheiro",
-        "escritorio": "Escritório",
-        "jardim": "Jardim",
-        "cozinha": "Cozinha",
-        "ferramentas": "Ferramentas",
-        "moveis": "Móveis",
-        "roupas": "Roupas",
-        "decoracao": "Decoração",
-        "automotivo": "Automotivo",
-        "games": "Games",
-        "brinquedos": "Brinquedos",
-        "praia": "Praia"
+        // Mapeamento de tags para nomes de categorias
+        eletrodomesticos: "Eletrodomésticos",
+        computador: "Computador",
+        celular: "Celular",
+        tech: "Tecnologia",
+        banheiro: "Banheiro",
+        escritorio: "Escritório",
+        jardim: "Jardim",
+        cozinha: "Cozinha",
+        ferramentas: "Ferramentas",
+        moveis: "Móveis",
+        roupas: "Roupas",
+        decoracao: "Decoração",
+        automotivo: "Automotivo",
+        games: "Games",
+        brinquedos: "Brinquedos",
+        praia: "Praia",
     };
 
-    // Obter a tag da URL
+    const categoriaTitulo = document.getElementById('categoria-titulo');
+    const produtoInterno = document.querySelector('.produto-interno');
+
     const params = new URLSearchParams(window.location.search);
     const tag = params.get('tag');
+    const pesquisa = params.get('pesquisa');
 
-    if (tag) {
-        // Atualizar o título da categoria
-        const categoriaTitulo = document.getElementById('categoria-titulo');
+    if (pesquisa) {
+        categoriaTitulo.textContent = `Pesquisa: "${pesquisa}"`;
+
+        // Carregar dados do JSON e filtrar os produtos pela pesquisa
+        fetch('./data/produtos.json')
+            .then(response => response.json())
+            .then(data => {
+                const produtos = data.listas[0].produtos;
+                const produtosFiltrados = produtos.filter(produto =>
+                    removeAcentos(produto.titulo.toLowerCase()).includes(removeAcentos(pesquisa.toLowerCase()))
+                );
+
+                renderizarProdutos(produtosFiltrados, pesquisa);
+            })
+            .catch(error => console.error('Erro ao carregar os dados do JSON:', error));
+    } else if (tag) {
         categoriaTitulo.textContent = categoriaNomes[tag] || "Categoria Desconhecida";
 
         // Carregar dados do JSON e filtrar os produtos pela tag
@@ -35,93 +49,66 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 const produtos = data.listas[0].produtos;
+                const produtosFiltrados = produtos.filter(produto => produto.tags.includes(tag));
 
-                // Função para renderizar os produtos na página
-                const renderizarProdutos = (produtosFiltrados, pesquisa) => {
-                    const produtoInterno = document.querySelector('.produto-interno');
-                    produtoInterno.innerHTML = ''; // Limpar conteúdo atual
-                    if (produtosFiltrados.length > 0) {
-                        produtosFiltrados.forEach(produto => {
-                            const produtoHTML = `
-                                <div class="produto-item">
-                                    <img src="${produto.imagem}" alt="${produto.titulo}">
-                                    <h2 class="produto-item-titulo">${produto.titulo}</h2>
-                                    <p class="produto-item-descricao">${produto.descricao}</p>
-                                    <div class="produto-item-estrelas">
-                                        ${'★'.repeat(produto.estrelas)}
-                                    </div>
-                                    <p class="produto-item-preco">${produto.preco}</p>
-                                    <button class="adicionarCarrinho" data-nome="${produto.titulo}" data-preco="${produto.preco}">
-                                        <i class="fas fa-shopping-cart"></i>
-                                    </button>
-                                </div>
-                            `;
-                            produtoInterno.insertAdjacentHTML('beforeend', produtoHTML);
-                        });
-                        categoriaTitulo.textContent = categoriaNomes[tag] || "Categoria Desconhecida";
-                    } else {
-                        produtoInterno.innerHTML = `
-                        <p class="texto-ajuda">Não encontrou o que deseja? Que tal pesquisar por todos os produtos!</p>
-                        <div class="ajuda-container">
-                            <input type="text" id="pesquisaTodos" placeholder="Pesquisar todos os produtos">
-                            <button id="pesquisarTodosBtn">
-                                <i class="fas fa-search"></i>
-                            </button>
-                        </div>
-                    `;
-                        const pesquisarTodosBtn = document.getElementById('pesquisarTodosBtn');
-                        const pesquisaTodosInput = document.getElementById('pesquisaTodos');
-                        pesquisarTodosBtn.addEventListener('click', () => {
-                            const pesquisaTodos = pesquisaTodosInput.value.trim().toLowerCase();
-                            const produtosFiltradosTodos = produtos.filter(produto =>
-                                produto.titulo.toLowerCase().includes(pesquisaTodos)
-                            );
-                            renderizarProdutos(produtosFiltradosTodos, pesquisaTodos);
-                            categoriaTitulo.textContent = `Pesquisa: "${pesquisaTodos}"`;
-                        });
-                    }
-                };
-
-                // Filtrar produtos com base na tag da categoria
-                let produtosFiltrados = produtos.filter(produto => produto.tags.includes(tag));
-
-                // Renderizar os produtos filtrados inicialmente
                 renderizarProdutos(produtosFiltrados, '');
-
-                // Adicionar event listener para o campo de pesquisa por nome
-                const pesquisaNomeInput = document.getElementById('pesquisaNome');
-                pesquisaNomeInput.addEventListener('input', () => {
-                    const pesquisaNome = pesquisaNomeInput.value.trim().toLowerCase();
-                    produtosFiltrados = produtos.filter(produto =>
-                        produto.titulo.toLowerCase().includes(pesquisaNome) && produto.tags.includes(tag)
-                    );
-                    renderizarProdutos(produtosFiltrados, pesquisaNome);
-                });
-
-                // Adicionar event listener para o campo de pesquisa por categoria
-                const pesquisaCategoriaInput = document.getElementById('pesquisaCategoria');
-                pesquisaCategoriaInput.addEventListener('input', () => {
-                    const pesquisaCategoria = pesquisaCategoriaInput.value.trim().toLowerCase();
-                    produtosFiltrados = produtos.filter(produto =>
-                        produto.tags.includes(tag) && produto.tags.some(tag => tag.includes(pesquisaCategoria))
-                    );
-                    renderizarProdutos(produtosFiltrados, pesquisaCategoria);
-                });
-
-                // Adicionar event listener para o campo de pesquisa por todos os produtos
-                const pesquisaTodosInput = document.getElementById('pesquisaTodos');
-                const pesquisarTodosBtn = document.getElementById('pesquisarTodosBtn');
-                pesquisarTodosBtn.addEventListener('click', () => {
-                    const pesquisaTodos = pesquisaTodosInput.value.trim().toLowerCase();
-                    const produtosFiltradosTodos = produtos.filter(produto =>
-                        produto.titulo.toLowerCase().includes(pesquisaTodos)
-                    );
-                    renderizarProdutos(produtosFiltradosTodos, pesquisaTodos);
-                    categoriaTitulo.textContent = `Pesquisa: "${pesquisaTodos}"`;
-                });
             })
             .catch(error => console.error('Erro ao carregar os dados do JSON:', error));
     } else {
         console.error('Nenhuma tag de categoria fornecida na URL.');
+
+        // Caso não haja tag nem pesquisa, mostrar mensagem de erro ou redirecionar para a página inicial, por exemplo
+        // window.location.href = '/';
     }
+
+    // Função para renderizar os produtos na página
+    const renderizarProdutos = (produtosFiltrados, pesquisa) => {
+        produtoInterno.innerHTML = ''; // Limpar conteúdo atual
+        if (produtosFiltrados.length > 0) {
+            produtosFiltrados.forEach(produto => {
+                const produtoHTML = `
+                    <div class="produto-item">
+                        <img class="produto-item-img" src="${produto.imagem}" alt="${produto.titulo}">
+                        <h2 class="produto-item-titulo">${produto.titulo}</h2>
+                        <p class="produto-item-descricao">${produto.descricao}</p>
+                        <div class="produto-item-estrelas">
+                            ${'★'.repeat(produto.estrelas)}
+                        </div>
+                        <p class="produto-item-preco">${produto.preco}</p>
+                        <button class="adicionarCarrinho" data-nome="${produto.titulo}" data-preco="${produto.preco}">
+                            <i class="fas fa-shopping-cart"></i>
+                        </button>
+                    </div>
+                `;
+                produtoInterno.insertAdjacentHTML('beforeend', produtoHTML);
+            });
+        } else {
+            produtoInterno.innerHTML = `
+                <p class="texto-ajuda">Não encontrou o que deseja? Que tal pesquisar por todos os produtos!</p>
+                <div class="ajuda-container">
+                    <input type="text" id="pesquisaTodos" placeholder="Pesquisar todos os produtos">
+                    <button id="pesquisarTodosBtn">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            `;
+            const pesquisarTodosBtn = document.getElementById('pesquisarTodosBtn');
+            const pesquisaTodosInput = document.getElementById('pesquisaTodos');
+            pesquisarTodosBtn.addEventListener('click', () => {
+                const pesquisaTodos = pesquisaTodosInput.value.trim().toLowerCase();
+                window.location.href = `categorias.html?pesquisa=${pesquisaTodos}`;
+            });
+            pesquisaTodosInput.addEventListener('keypress', (event) => {
+                if (event.key === 'Enter') {
+                    const pesquisaTodos = pesquisaTodosInput.value.trim().toLowerCase();
+                    window.location.href = `categorias.html?pesquisa=${pesquisaTodos}`;
+                }
+            });
+        }
+    };
+
+    // Função para remover acentos de uma string
+    const removeAcentos = (str) => {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    };
 });
--- js/mainJS/header.js
+++ js/mainJS/header.js
@@ -1,7 +1,110 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const categoriaNomes = {
+        // Mapeamento de tags para nomes de categorias
+        eletrodomesticos: "Eletrodomésticos",
+        computador: "Computador",
+        celular: "Celular",
+        tech: "Tecnologia",
+        banheiro: "Banheiro",
+        escritorio: "Escritório",
+        jardim: "Jardim",
+        cozinha: "Cozinha",
+        ferramentas: "Ferramentas",
+        moveis: "Móveis",
+        roupas: "Roupas",
+        decoracao: "Decoração",
+        automotivo: "Automotivo",
+        games: "Games",
+        brinquedos: "Brinquedos",
+        praia: "Praia",
+    };
 
-    var menu = document.querySelector('.menuMobile');
+    const categoriaIcones = {
+        // Mapeamento de tags para ícones de categorias
+        eletrodomesticos: "./icons/ico-eletro.svg",
+        computador: "./icons/ico-computador.svg",
+        celular: "./icons/ico-celular.svg",
+        tech: "./icons/ico-tech.svg",
+        banheiro: "./icons/ico-banheiro.svg",
+        escritorio: "./icons/ico-escritorio.svg",
+        jardim: "./icons/ico-jardim.svg",
+        cozinha: "./icons/ico-cozinha.svg",
+        ferramentas: "./icons/ico-ferramentas.svg",
+        moveis: "./icons/ico-moveis.svg",
+        roupas: "./icons/ico-roupas.svg",
+        decoracao: "./icons/ico-decoracao.svg",
+        automotivo: "./icons/ico-automotivo.svg",
+        games: "./icons/ico-games.svg",
+        brinquedos: "./icons/ico-brinquedos.svg",
+        praia: "./icons/ico-praia.svg",
+    };
 
-menu.addEventListener('click', function() {
-    var isExpanded = menu.getAttribute('aria-expanded') === 'true';
-    menu.setAttribute('aria-expanded', !isExpanded);
+    const headerSearchInput = document.querySelector(".search input");
+    const suggestionsContainer = document.createElement("div");
+    suggestionsContainer.classList.add("suggestions-container");
+    document.body.appendChild(suggestionsContainer);
+
+    // Função para renderizar sugestões de categorias
+    const renderizarSugestoes = () => {
+        suggestionsContainer.innerHTML = ""; // Limpar sugestões anteriores
+        for (const [key, value] of Object.entries(categoriaNomes)) {
+            const suggestionItem = document.createElement("div");
+            suggestionItem.classList.add("suggestion-item");
+            suggestionItem.innerHTML = `<img src="${categoriaIcones[key]}" alt="${value} Icon">${value}`;
+            suggestionItem.dataset.tag = key; // Adicionar a tag como um atributo de dados
+            suggestionsContainer.appendChild(suggestionItem);
+        }
+        suggestionsContainer.style.display = "block";
+        suggestionsContainer.style.left =
+            headerSearchInput.getBoundingClientRect().left + "px";
+        suggestionsContainer.style.top =
+            headerSearchInput.getBoundingClientRect().bottom + "px";
+        suggestionsContainer.style.width = headerSearchInput.offsetWidth + "px";
+    };
+
+    headerSearchInput.addEventListener("focus", renderizarSugestoes); // Renderizar sugestões ao focar no input
+
+    document.addEventListener("click", (event) => {
+        if (
+            !suggestionsContainer.contains(event.target) &&
+            event.target !== headerSearchInput
+        ) {
+            suggestionsContainer.style.display = "none";
+        }
+    });
+
+    suggestionsContainer.addEventListener("click", (event) => {
+        if (
+            event.target.classList.contains("suggestion-item") ||
+            event.target.tagName === "IMG"
+        ) {
+            const suggestionItem = event.target.classList.contains("suggestion-item")
+                ? event.target
+                : event.target.parentElement;
+            const selectedCategory = suggestionItem.dataset.tag;
+            headerSearchInput.value = ""; // Limpar o input para evitar que a categoria seja enviada como termo de pesquisa
+            headerSearchInput.value = suggestionItem.textContent;
+            suggestionsContainer.style.display = "none";
+            window.location.href = `categorias.html?tag=${selectedCategory}`;
+        }
+    });
+
+    const headerSearchButton = document.querySelector(".search button");
+    headerSearchButton.addEventListener("click", () => {
+        const pesquisa = headerSearchInput.value.trim().toLowerCase();
+        if (pesquisa.length > 0) { // Verificar se a pesquisa não está vazia
+            headerSearchInput.value = ""; // Limpar o input
+            window.location.href = `categorias.html?pesquisa=${pesquisa}`;
+        }
+    });
+
+    headerSearchInput.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            const pesquisa = headerSearchInput.value.trim().toLowerCase();
+            if (pesquisa.length > 0) { // Verificar se a pesquisa não está vazia
+                headerSearchInput.value = ""; // Limpar o input
+                window.location.href = `categorias.html?pesquisa=${pesquisa}`;
+            }
+        }
+    });
 });
